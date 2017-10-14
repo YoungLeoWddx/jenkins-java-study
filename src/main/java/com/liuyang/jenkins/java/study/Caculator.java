c94a491ec9524364069799dcc5c31dd393de5f12
YoungLeoWddx <773958125@qq.com>
2017-10-14 15:16:21 +0800

    777：添加了javadoc插件。

--- src/main/java/com/liuyang/jenkins/java/study/Caculator.java
+++ src/main/java/com/liuyang/jenkins/java/study/Caculator.java
@@ -1,11 +1,30 @@
 package com.liuyang.jenkins.java.study;
 
+
+/**
+ * Description:
+ * 计算器类
+ * @author Leo
+ * @version V1.0  
+ * created on 2017年10月14日下午3:13:05
+ */
 public class Caculator {
-	
+	/**
+	 * 加法运算
+	 * @param a  第一个被加数
+	 * @param b  第二个被加数
+	 * @return  返回求和的结果
+	 */
 	public int add(int a,int b){
 		return a + b;
 	}
 	
+	/**
+	 * 减法运算
+	 * @param a 被减数
+	 * @param b 减数
+	 * @return 返回求差的结果
+	 */
 	public int min(int a,int b){
 		if(a < b){
 			return a;
@@ -14,6 +33,12 @@ public class Caculator {
 		return b;
 	}
 	
+	/**
+	 * 求两个整数中的最大值
+	 * @param a 比较数a
+	 * @param b 比较数b
+	 * @return 返回最大的结果
+	 */
 	public int max(int a, int b){
 		if(a > b){
 			return a;
@@ -21,6 +46,11 @@ public class Caculator {
 		return b;
 	}
 	
+	/**
+	 * 根据输入的月份数字，返回月份的字符串
+	 * @param month 月份的数字
+	 * @return 返回对应月份数字的字符串
+	 */
 	public String toSeason(int month){
 		String season = null;
 		switch (month) {
